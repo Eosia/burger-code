7db258ac2551fb675042fedaf90840a5140d92e7
kevin <kevin.eosiaweb@gmail.com>
2021-11-23 22:54:53 +0100

    base html

--- index.php
+++ index.php
@@ -1,14 +1,109 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Burger code</title>
+    <!--bootstrap css-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <!--bootstrap css/-->
+
+
 </head>
 <body>
-    
+
+    <!--container principal-->
+    <div class="container site">
+
+        <!--titre-->
+        <h1 class="text-logo">
+            <i class="fas fa-utensils text-warning"></i>
+            <span class="mx-2">Burger Code</span>
+            <i class="fas fa-utensils text-warning"></i>
+        </h1>
+        <!--titre/-->
+        
+        <!--menu de navigation-->
+        <nav>
+            <ul class="text-uppercase text-decoration-none list-unstyled">
+                <!--lien1-->
+                <li role="presentation" class="active">
+                    <a href="#1" data-toggle="tab">
+                        Menus
+                    </a>
+                </li>
+                <!--lien1/-->
+
+                <!--lien2-->
+                <li role="presentation">
+                    <a href="#2" data-toggle="tab">
+                        Burgers
+                    </a>
+                </li>
+                <!--lien2/-->
+
+                <!--lien3-->
+                <li role="presentation">
+                    <a href="#3" data-toggle="tab">
+                        Snacks
+                    </a>
+                </li>
+                <!--lien3/-->
+
+                <!--lien4-->
+                <li role="presentation">
+                    <a href="#4" data-toggle="tab">
+                        Salades
+                    </a>
+                </li>
+                <!--lien4/-->
+
+                <!--lien5-->
+                <li role="presentation">
+                    <a href="#5" data-toggle="tab">
+                        Boissons
+                    </a>
+                </li>
+                <!--lien5/-->
+
+                <!--lien6-->
+                <li role="presentation" class="active">
+                    <a href="#6" data-toggle="tab">
+                        Desserts
+                    </a>
+                </li>
+                <!--lien6/-->
+            </ul>
+        </nav>
+        <!--menu de navigation/-->
+
+        <div class="tab-content">
+
+            <div class="tab-pane active" id="1">
+                <div class="row">
+                    <div class="col-sm-6">
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
 
 
+    </div>
+    <!--container principal/-->
+
+
+    <!--fontawesome-->
+    <script src="https://kit.fontawesome.com/11070bdd9d.js" crossorigin="anonymous"></script>
+    <!--fontawesome/-->
+
+    <!--bootstrap script-->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    <!--bootstrap script/-->
 </body>
 </html>
